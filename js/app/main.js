069e306be55fca6e9af61588c7a726e5c2c2965d
PKU7 <PKU7@users.noreply.github.com>
2016-01-08 16:43:15 -0500

    Delete main.js

--- js/app/main.js
+++
@@ -1,7 +0,0 @@
-define(["jquery", "jquery.single-page", "bootstrap"], function() {
-    //the jquery.nav.js and jquery.footer.js plugins have been loaded.
-    jQuery(function() {
-        jQuery(".navbar").singlePage();
-        jQuery(".carousel").carousel();
-    });
-});
